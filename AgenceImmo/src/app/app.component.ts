ed776de3de6ad453413a61a128f64fcee1351b10
bbourger <bbourger@unis.fr>
2019-04-11 10:35:18 +0200

    ajout de la config de firebase

--- AgenceImmo/src/app/app.component.ts
+++ AgenceImmo/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AgenceImmo';
+  
+  constructor() {
+    // Initialize Firebase
+  const config = {
+    apiKey: 'AIzaSyC0B00CJ7RCus3eiMFll3HRqcHm3P2US0k',
+    authDomain: 'agenceimmo-236db.firebaseapp.com',
+    databaseURL: 'https://agenceimmo-236db.firebaseio.com',
+    projectId: 'agenceimmo-236db',
+    storageBucket: 'agenceimmo-236db.appspot.com',
+    messagingSenderId: '302569164168'
+  };
+  firebase.initializeApp(config);
+  }
+
 }
